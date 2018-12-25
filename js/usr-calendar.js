0a96a58e1ffd6663cfd044fb52caa202220b1518
minabe <minabe@bringup.jp>
2018-12-25 15:11:14 +0900

    Change usr-calendar.js

--- js/usr-calendar.js
+++ js/usr-calendar.js
@@ -7,19 +7,43 @@ var usr_calendar = new Vue({
                 subject: 'テスト1',
                 start_date: '2018-12-03',
                 end_date: '2018-12-03',
-                link: 'http://localhost:8080/forval_nippin_sy/site/'
+                link: 'http://localhost:8080/'
             },
             {
                 subject: 'テスト2',
                 start_date: '2018-12-03',
                 end_date: '2018-12-03',
-                link: 'http://localhost:8080/forval_nippin_sy/site/'
+                link: 'http://localhost:8080/'
             },
             {
                 subject: 'テスト3',
+                start_date: '2018-12-03',
+                end_date: '2018-12-03',
+                link: 'http://localhost:8080/'
+            },
+            {
+                subject: 'テスト4',
+                start_date: '2018-12-03',
+                end_date: '2018-12-03',
+                link: 'http://localhost:8080/'
+            },
+            {
+                subject: 'テスト5',
+                start_date: '2018-12-03',
+                end_date: '2018-12-03',
+                link: 'http://localhost:8080/'
+            },
+            {
+                subject: 'テスト6',
+                start_date: '2018-12-04',
+                end_date: '2018-12-04',
+                link: 'http://localhost:8080/'
+            },
+            {
+                subject: 'テスト',
                 start_date: '2018-12-17',
-                end_date: '2018-12-25',
-                link: 'http://localhost:8080/forval_nippin_sy/site/'
+                end_date: '2018-12-28',
+                link: 'http://localhost:8080/'
             }
         ];
         this.setCalendar();
